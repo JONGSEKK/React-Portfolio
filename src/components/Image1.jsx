b22975d8531a4160645adca715f3fd610951c656
JONGSEKK <rikeyk71@gmail.com>
2023-09-04 16:19:41 +0800

    s

--- src/components/Image1.jsx
+++ src/components/Image1.jsx
@@ -2,7 +2,7 @@ import { Parallax } from 'react-parallax';
 import nasa from '../assets/nasa.jpg'
 
 const Image1 = () => (
-    <Parallax className='relative h-screen' bgImage={nasa} strength={700}>
+    <Parallax className='relative h-screen' bgImage={nasa} strength={1000}>
         <div className='flex items-center justify-center absolute h-screen w-full'>
             <span className='text-white bg-[#333] p-4 text-2xl tracking-[10px] uppercase'>Learning Scroll Parallax</span>
         </div>
